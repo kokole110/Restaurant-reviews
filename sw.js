55588e9c72840582aec427c2e3c4c9560c9600ec
Kokole <kokole110@gmail.com>
2019-05-09 16:54:12 +0300

    bug with serviceworker is fixed

--- sw.js
+++ sw.js
@@ -49,8 +49,7 @@ self.addEventListener('fetch', function(event){
   event.respondWith(
     caches.match(event.request)
     .then(function(response){
-      if(response)return response;
-      fetch(event.request);
+      return response || fetch(event.request)
     })
   );
 });
